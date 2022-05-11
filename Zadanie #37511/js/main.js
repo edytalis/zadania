8453aa61ed9805f69f911ca8734f0b10d0662c89
edytalis <edyta.maria.lis@gmail.com>
2022-05-11 10:08:57 +0200

    value zadanie do weryfikacji

---
+++ Zadanie #37511/js/main.js
@@ -0,0 +1,20 @@
+/* let nazwa = document.getElementById("name");
+console.log(fname.value); */
+
+let button = document.querySelector("button");
+
+
+function getdatafrom() {
+    let inputfname = document.getElementById("fname").value;
+    let inputlname = document.getElementById("lname").value;
+    let inputphone = document.getElementById("phone").value;
+  console.log(inputphone);
+  
+  let parname = document.getElementById("par_name");
+  let parlname = document.getElementById("par_lname");
+  let parphone = document.getElementById("par_phone");
+
+  inputfname = parname
+}
+  
+button.addEventListener('click', getdatafrom);
